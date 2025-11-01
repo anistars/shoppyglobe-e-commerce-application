79240c05276892403be8c9594e72695df30aaccc
Aniket Rahalkar <aniket.rahalkar99@gmail.com>
2025-11-01 11:43:26 +0530

    Added the counter and add to cart button for adding product to cart

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,14 +1,22 @@
 import { useEffect, useState } from 'react';
+import '../index.css';
 
 function ProductList() {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [cartCounts, setCartCounts] = useState({});
+    const [message, setMessage] = useState('');
 
     useEffect(() => {
         fetch('https://dummyjson.com/products')
             .then(res => res.json())
             .then(data => {
                 setProducts(data);
+                const intialQuantitys = {};
+                data.products.forEach(product => {
+                    intialQuantitys[product.id] = 0;
+                });
+                setCartCounts(intialQuantitys);
                 setLoading(false);
             })
             .catch(error => {
@@ -16,9 +24,36 @@ function ProductList() {
                 setLoading(false);
             });
     }, []);
+
+    const handleQuantityChange = (productId, delta) => {
+        setCartCounts(prevCounts => ({
+            ...prevCounts,
+            [productId]: Math.max(1, (prevCounts[productId] || 0) + delta)
+        }));
+    };
+    const handleAddToCart = (productId) => {
+        const quantity = cartCounts[productId] || 0;
+        const productTitle = products.products.find(p => p.id === productId)?.title || 'Product';
+        if (quantity === 0) {
+            setMessage(`⚠️ Please select at least 1 item of "${productTitle}" to add to cart`);
+            setTimeout(() => setMessage(''), 3000);
+            return;
+        }
+        setMessage(`✅ Added ${quantity} of "${productTitle}" to cart`);
+        setTimeout(() => setMessage(''), 3000);
+
+    }
     return (
-        <div className="container mt-4">
+        <div className="container mt-5 pt-5">
             <h2 className="mb-4">Product List</h2>
+            <div className="position-relative">
+                {message && (
+                    <div className="floating-message alert alert-success text-center">
+                        {message}
+                    </div>
+                )}
+            </div>
+
             {loading ? (
                 <p>Loading products...</p>
             ) : (
@@ -26,15 +61,38 @@ function ProductList() {
                     {products.products && products.products.length > 0 ? (
                         products.products.map(product => (
                             <div className="col-md-4 mb-4" key={product.id}>
-                                <div className="card h-100">
-                                    <img src={product.thumbnail} className="card-img-top" alt={product.title} />
-                                    <div className="card-body">
+                                <div className="card h-100 product-card shadow-sm" style={{ backgroundColor: "#f3f6f4" }}>
+                                    <img
+                                        src={product.thumbnail}
+                                        className="card-img-top product-image"
+                                        alt={product.title}
+                                    />
+                                    <div className="card-body d-flex flex-column justify-content-between text-center">
                                         <h5 className="card-title">{product.title}</h5>
-                                        <p className="card-text">{product.description}</p>
-                                        <p className="card-text">Price: ${product.price}</p>
+                                        <p className="card-text line-clamp">{product.description}</p>
+                                        <p className="card-text fw-bold text-success">Price: ${product.price}</p>
+                                        <div className="d-flex justify-content-center align-items-center my-3">
+                                            <button className="btn btn-outline-secondary btn-sm"
+                                                onClick={() => handleQuantityChange(product.id, -1)}
+                                                disabled={cartCounts[product.id] <= 1}
+                                            ><i class="bi bi-dash-circle"></i></button>
+                                            <span className="mx-3 fs-5">{cartCounts[product.id] || 0}</span>
+                                            <button className="btn btn-outline-secondary btn-sm"
+                                                onClick={() => handleQuantityChange(product.id, 1)}
+                                            ><i class="bi bi-plus-circle"></i></button>
+                                        </div>
+
+                                        <button className="btn btn-primary mx-auto"
+                                            onClick={() => handleAddToCart(product.id)}
+                                        >
+                                            Add to Cart ({cartCounts[product.id] || 0})
+
+                                        </button>
+
                                     </div>
                                 </div>
                             </div>
+
                         ))
                     ) : (
                         <p>No products found.</p>
